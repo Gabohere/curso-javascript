c818a5bbeb75a2c88253cb698a5d45308c441ece
gabriel <gabrielherencia69@gmail.com>
2022-03-19 21:58:55 -0500

    css y formulas con condigos de descuento

--- descuentos.js
+++ descuentos.js
@@ -1,5 +1,19 @@
 const precioOriginal = 260;
 const descuento = 18;
+const coupons = [
+    {
+        name: "GABO",
+        discount: 10,
+    },
+    {
+        name: "GABY",
+        discount: 30,
+    },
+    {
+        name: "AMBOS",
+        discount: 50,
+    }
+];
 
 function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
@@ -10,11 +24,25 @@ function calcularPrecioConDescuento(precio, descuento) {
 
 function onClickButtonPriceDiscount() {
     const inputPrice = document.getElementById("inputPrice");
-    const onputDiscount = document.getElementById("inputDiscount");
+    const priceValue = inputPrice.value;
+
+    const inputCoupon = document.getElementById("inputCoupon");
+    const couponValue = inputCoupon.value;
+
+    const isCouponValueValid = function (coupon) {
+        return coupon.name === couponValue;
+    };
+
+    const userCoupon = coupons.find(isCouponValueValid);
+
+    if (!userCoupon) {
+        alert("El cupón " + couponValue + " no es válido");
+    } else {
+        const descuento = userCoupon.discount;
+        const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+        const priceResult = document.getElementById("priceResult");
+        priceResult.innerText = "El precio con descuento de tus batatabits es: $ " + precioConDescuento;
+    }
 }
-// console.log({
-//     precioOriginal,
-//     descuento,
-//     porcentajePrecioConDescuento,
-//     precioConDescuento,
-// });
+
